01abcc0bc5dadc3e0c36a243e96d1e4283ea455b
Annie Hughey <agh87@cornell.edu>
2016-06-07 23:22:26 -0700

    Added trend analysis start/STD

--- perfmon.sh
+++ perfmon.sh
@@ -31,19 +31,15 @@ do
 	CURRENT_VF=$(top -b -n1 | grep $PROCESS | head -1 | awk '{print $5}')
 	PROC_PID=$(top -b -n1 | grep $PROCESS | head -1 | awk '{print $1}')
 			
-	#CPU_ARRAY+=$CURRENT_CPU
-	#MEM_ARRAY+=$CURRENT_CPU
-	#VF_ARRAY+=$CURRENT_CPU
+	CPU_ARRAY[$COUNTER]=($CURRENT_CPU)
+	MEM_ARRAY[$COUNTER]=($CURRENT_MEM)
+	VF_ARRAY[$COUNTER]=($CURRENT_VF)
+
+	echo "CPU: $CPU_ARRAY   MEM: $MEM_ARRAY   VF: $VF_ARRAY"
 		
 	CPU_SUM=$(echo "$CPU_SUM+$CURRENT_CPU" | bc -l)
 	MEM_SUM=$(echo "$MEM_SUM+$CURRENT_MEM" | bc -l)
 	VF_SUM=$(echo "$VF_SUM+$CURRENT_VF" | bc -l)
-
-	echo "CPU SUM: $CPU_SUM MEM SUM: $MEM_SUM VF_SUM: $VF_SUM"
-
-	#R_AVG_CPU=$(echo $CPU_SUM $COUNTER1 | awk '{printf "%4.3f\n",$1/$2}')
-	#R_AVG_MEM=$(echo $MEM_SUM $COUNTER1 | awk '{printf "%4.3f\n",$1/$2}')
-	#R_AVG_VF=$(echo $VF_SUM $COUNTER1 | awk '{printf "%4.3f\n",$1/$2}')
 
 	R_AVG_CPU=$(echo "$CPU_SUM/$COUNTER1" | bc -l)
 	R_AVG_MEM=$(echo "$MEM_SUM/$COUNTER1" | bc -l)
@@ -53,24 +49,40 @@ do
 	#R_STD_MEM=
 	#R_STD_VF=	
 
-	echo "POLL NUMBER: $COUNTER1"
+	echo -e "\nPOLL NUMBER: $COUNTER1"
 	echo "Process PID: $PROC_PID "
-	echo "Current CPU: $CURRENT_CPU %"
+	echo -e "\nCurrent CPU: $CURRENT_CPU %"
 	echo "Rolling AVG CPU: $R_AVG_CPU %"
-	echo "Current MEM: $CURRENT_MEM %"
+	echo "Rolling STD CPU: $R_STD_CPU "
+	echo -e "\nCurrent MEM: $CURRENT_MEM %"
 	echo "Rolling AVG MEM: $R_AVG_MEM %"
-	echo "Current Virtual Footprint: $CURRENT_VF"
+	echo "Rolling STD MEM: $R_STD_MEM "
+	echo -e "\nCurrent Virtual Footprint: $CURRENT_VF"
 	echo "Rolling AVG Virtual Footprint: $R_AVG_VF " 
-	
+	echo "Rolling STD Virtual Footprint: $R_STD_VF "
 	sleep $POLLINT
 	let COUNTER=COUNTER+1
 	let COUNTER1=COUNTER1+1
 done
 
-if [[ $COUNTER -eq $RUN_NUM ]]; then 
-	echo "Final CPU average: $R_AVG_CPU %"
-	echo "Final Memory average: $R_AVG_MEM %"
-	echo "Final Virtual Footprint average: $R_AVG_VF "
-	echo "Final STD:   "
-	echo "Trend Analysis"
-fi
+echo -e "$CPU_ARRAY\n$MEM_ARRAY\n$VF_ARRAY"
+L_of_L=($CPU_ARRAY,$MEM_ARRAY,$VF_ARRAY)
+diff_CPU=0
+diff_MEM=0
+diff_VF=0
+for list in list of list; do
+	for e in list:
+		if list == CPU_ARRAY:
+			diffCPU=list[e+1]-list[e]
+		if list == MEM_ARRAY
+			diff_MEM=list[e+1]-list[e]
+		if list == VF_ARRAY
+			diff_VF=list[e+1]-list[e]
+
+echo -e "\n"	
+echo "Final CPU average: $R_AVG_CPU %"
+echo "Final Memory average: $R_AVG_MEM %"
+echo "Final Virtual Footprint average: $R_AVG_VF "
+
+
+
